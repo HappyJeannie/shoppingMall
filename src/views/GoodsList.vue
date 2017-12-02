01a3c34460039046530acefa11245d33665a5342
jeannie <436030684@qq.com>
2017-12-02 23:32:55 +0800

    add

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    这是商品列表页面
+    <div>{{$route.params.goodsId}}</div>
+    <div>{{$route.params.name}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
